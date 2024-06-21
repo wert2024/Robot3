2bf82c14d838409272e2588b276ab15b9435aeec
wert2024 <158856496+wert2024@users.noreply.github.com>
2024-06-21 19:35:04 +0300

    Add files via upload

---
+++ калькулятор.js
@@ -0,0 +1,86 @@
+jQuery('document').ready(function(){
+
+
+	$
+	
+
+	
+	console.log(0);
+	
+
+$.ajax('https://wert2024.github.io/Robot3/', {
+	success: function(){
+		console.log('I get response');
+		function(data){
+			console.log(data);
+			console.log(data + '1');
+			console.log(2 + 2);
+		}
+	}
+});
+	
+	
+	/*
+ document.querySelector('.btn').onclick = function () {
+ let count = document.getElementById('count');
+ let value = count.innerHTML;
+ ++value;
+ console.log(value);
+ document.getElementById('count').innerHTML = value;
+ }
+*/
+ 
+
+
+
+ 
+
+
+//------------------------------------------
+  
+	/*
+	//сервер работает
+console.log(0);
+
+$.ajax('https://jsonplaceholder.typicode.com/posts', {
+	success: function(){
+		console.log('I get response');
+		document.querySelector('.btn').onclick = function () {
+ let count = document.getElementById('count');
+ let value = count.innerHTML;
+ ++value;
+ console.log(value);
+ document.getElementById('count').innerHTML = value;
+ }
+console.log('I get response2');
+	}
+});
+
+console.log(1);	
+console.log(2);
+console.log(3);	
+console.log(4);
+console.log(5);      
+*/      
+ 
+     
+     
+     });
+ 
+     
+     
+
+
+
+  
+     
+
+
+
+
+
+
+
+
+
+
